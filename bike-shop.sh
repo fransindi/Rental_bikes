57f6693aef0639700ec090b42ec510aebda9474f
fransindi <frangarciasamat@gmail.com>
2023-09-19 21:04:27 -0400

    Save progress: 1530.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -102,6 +102,7 @@ RETURN_MENU() {
     
     else    
         #get customer's rentals
+        CUSTOMER_RENTALS=$($PSQL "select bike_id, type, size from bikes inner join rentals using(bike_id) inner join customers using(customer_id) where phone = '$PHONE_NUMBER' and date_returned is null order by bike_id")
         #if no rentals
         #send to main menu
     fi
